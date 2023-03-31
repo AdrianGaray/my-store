f61de9a124a2a2d4a71425c87de4fdd6526421f3
AdrianGaray <adrgaray@emeal.nttdata.com>
2023-03-31 11:11:41 -0300

    Configurar eslint y prettier en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,32 @@ import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   widthImg = 10;
   name = 'Adri';
   age = 18;
-  img = 'https://images.unsplash.com/photo-1602572878641-973c1b8679e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3OTI2NjcyMg&ixlib=rb-4.0.3&q=80&w=1080'
+  img =
+    'https://images.unsplash.com/photo-1602572878641-973c1b8679e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3OTI2NjcyMg&ixlib=rb-4.0.3&q=80&w=1080';
   btnDisabled = true;
   // Se declara un objeto Persona
   person = {
     name: 'Adri',
     age: 18,
-    avatar: 'https://images.unsplash.com/photo-1602572878641-973c1b8679e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3OTI2NjcyMg&ixlib=rb-4.0.3&q=80&w=1080'
-  }
+    avatar:
+      'https://images.unsplash.com/photo-1602572878641-973c1b8679e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3OTI2NjcyMg&ixlib=rb-4.0.3&q=80&w=1080',
+  };
 
-  toggleButton(){
+  toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
 
-  increaseAge(){
-    this.person.age +=1;
+  increaseAge() {
+    this.person.age += 1;
   }
 
-  onScroll(event: Event){
+  onScroll(event: Event) {
     //le indicas a Angular que lo trate como un HTMLElement.
     //Así te dará propiedades y funciones con las cuales poder trabajar al colocar un . después de la variable que tiene la referencia del event.target
     const element = event.target as HTMLElement;
@@ -37,25 +39,25 @@ export class AppComponent {
     console.log(element.scrollTop);
   }
 
-  changeName(event: Event){
+  changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
   }
 
   // creacion de un array
-  names: string[] = ['Nico','Juli','Santi'];
+  names: string[] = ['Nico', 'Juli', 'Santi'];
   newName = '';
 
-  addName(){
+  addName() {
     this.names.push(this.newName);
-    this.newName='';
+    this.newName = '';
   }
 
-  deleteName(index: number){
+  deleteName(index: number) {
     this.names.splice(index, 1);
   }
 
-  products: Product []= [
+  products: Product[] = [
     {
       name: 'EL mejor juguete',
       price: 565,
@@ -65,41 +67,41 @@ export class AppComponent {
     {
       name: 'Bicicleta casi nueva',
       price: 356,
-      image: './assets/images/bike.jpg'
+      image: './assets/images/bike.jpg',
     },
     {
       name: 'Colleción de albumnes',
       price: 34,
-      image: './assets/images/album.jpg'
+      image: './assets/images/album.jpg',
     },
     {
       name: 'Mis libros',
       price: 23,
-      image: './assets/images/books.jpg'
+      image: './assets/images/books.jpg',
     },
     {
       name: 'Casa para perro',
       price: 34,
-      image: './assets/images/house.jpg'
+      image: './assets/images/house.jpg',
     },
     {
       name: 'Gafas',
       price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ]
+      image: './assets/images/glasses.jpg',
+    },
+  ];
 
   box = {
     width: 100,
     height: 100,
-    background: 'red'
+    background: 'red',
   };
 
   register = {
     name: '',
     email: '',
     password: '',
-  }
+  };
 
   onRegister() {
     console.log(this.register);
--- src/app/recap.ts
+++ src/app/recap.ts
@@ -1,19 +1,17 @@
 const empresa: string = 'Platzi';
 const id: number = 12;
-
 const sum = (a: number, b: number) => {
   return a + b;
-}
+};
 
-sum(1,3);
+sum(1, 3);
 
-class Persona{
-  constructor(public age: number, public lastname: string){ }
+class Persona {
+  constructor(public age: number, public lastname: string) {}
 }
 
 const adri = new Persona(13, 'garaycho');
 
-
-const suma = ( a : number , b: number):number => a + b
-const saludar = (nombre: string , edad: number ):string => `Hola me llamo ${nombre} y tengo ${edad} años`
-
+const suma = (a: number, b: number): number => a + b;
+const saludar = (nombre: string, edad: number): string =>
+  `Hola me llamo ${nombre} y tengo ${edad} años`;
